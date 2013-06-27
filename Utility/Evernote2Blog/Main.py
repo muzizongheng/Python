254d218e7137cb8728349494585cde0f3b6d7432
muzizongheng <jiangong.li@united-imaging.com>
2013-06-27 16:33:41 +0800

    Add main process

---
+++ Utility/Evernote2Blog/Main.py
@@ -0,0 +1,8 @@
+import AccessEN
+import AccessBlog
+
+#Use this tool to get notes from evernote, and publish these notes to blog
+#Author: Li Jiangong
+#Email: jgli_2008@sina.com
+
+exec("AccessEn.py")
