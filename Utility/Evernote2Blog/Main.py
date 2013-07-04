c9c4363dfb48cf9b2a270ee89842e71be4d2140e
muzizongheng <jiangong.li@united-imaging.com>
2013-07-04 10:09:07 +0800

    format code

--- Utility/Evernote2Blog/Main.py
+++ Utility/Evernote2Blog/Main.py
@@ -12,7 +12,7 @@ import json
 from datetime import datetime
 
 if "..\\Evernote\\lib" not in sys.path:
-	sys.path.append("..\\Evernote\\lib")
+    sys.path.append("..\\Evernote\\lib")
 
 import thrift.protocol.TBinaryProtocol as TBinaryProtocol
 import thrift.transport.THttpClient as THttpClient
@@ -53,6 +53,9 @@ metaweblog = evernote.initBlog()
 notebooks = noteStore.listNotebooks(authToken)
 print("Found ", len(notebooks), " notebooks:")
 
+#calculate processed blog count currently
+processedBlogCount = 0
+
 for notebook in notebooks:
     print("  * ", notebook.name)
 
@@ -69,112 +72,115 @@ for notebook in notebooks:
     nextOffset = 0
 
     while nextOffset <  noteCount:
-	    noteList = noteStore.findNotes(authToken, filter, nextOffset, noteCount)
-	    print("Get %i notes, currrent note offset: %i\n"%(len(noteList.notes), nextOffset))
+        noteList = noteStore.findNotes(authToken, filter, nextOffset, noteCount)
+        print("Get %i notes, currrent note offset: %i\n"%(len(noteList.notes), nextOffset))
 
-	    #increase offset for next find
-	    nextOffset += len(noteList.notes)
+        #increase offset for next find
+        nextOffset += len(noteList.notes)
 
-	    #create blog category by tags
-	    if evernote.isCreateTags == True:			
-		    tagslist = noteStore.listTagsByNotebook(authToken, notebook.guid)
-		    categories = convertTags2Category(tagslist)
+        #create blog category by tags
+        if evernote.isCreateTags == True:           
+            tagslist = noteStore.listTagsByNotebook(authToken, notebook.guid)
+            categories = convertTags2Category(tagslist)
 
-		    try:
-		        for c in categories:
-		            metaweblog.new_category(c)
-		            print("create blog's category: ", c.name)
-		    except Exception as err:
-		        print("Create category failed: ", err)
-		    finally:
-		        pass
+            try:
+                for c in categories:
+                    metaweblog.new_category(c)
+                    print("create blog's category: ", c.name)
+            except Exception as err:
+                print("Create category failed: ", err)
+            finally:
+                pass
 
-	    #print noteList
-	    for n in noteList.notes:
-	        print(n.title)
+        #print noteList
+        for n in noteList.notes:
+            print(n.title)
 
-	        if (n.title+'\n' in currentBlogs) | (n.title in currentBlogs):
-	            continue
+            if (n.title+'\n' in currentBlogs) | (n.title in currentBlogs):
+                continue
 
-	        #get note raw content
-	        content = noteStore.getNoteContent(authToken, n.guid)
-	        print("get note(%s) content successfuly"%(n.title))
+            #get note raw content
+            content = noteStore.getNoteContent(authToken, n.guid)
+            print("get note(%s) content successfuly"%(n.title))
 
-	        #apply tags to blog
-	        tags = noteStore.getNoteTagNames(authToken, n.guid)
-	        print("get note(%s) tags successfuly"%(n.title))
+            #apply tags to blog
+            tags = noteStore.getNoteTagNames(authToken, n.guid)
+            print("get note(%s) tags successfuly"%(n.title))
 
-	        #get resource & write to local    
-	        try:
-	            if n.resources is not None:
-	                for res in n.resources:
-	                    print("guid is: " + res.guid)
-	                    print("width is: ", res.width)
-	                    print("height is: ", res.height)
-	                    print("resource type is: ", res.mime)
+            #get resource & write to local    
+            try:
+                if n.resources is not None:
+                    for res in n.resources:
+                        print("guid is: " + res.guid)
+                        print("width is: ", res.width)
+                        print("height is: ", res.height)
+                        print("resource type is: ", res.mime)
 
-	                    attachment = noteStore.getResource(authToken, res.guid, True, False, True, False)
+                        attachment = noteStore.getResource(authToken, res.guid, True, False, True, False)
 
-	                    #fileType = getMediaType(res.mime)
-	                    # attachmentFile = open(res.guid+"."+fileType, "wb")
-	                    # attachmentFile.write(attachment.data.body)
-	                    # attachmentFile.close()
+                        #fileType = getMediaType(res.mime)
+                        # attachmentFile = open(res.guid+"."+fileType, "wb")
+                        # attachmentFile.write(attachment.data.body)
+                        # attachmentFile.close()
 
-	                    #upload resource to blog
-	                    fileData = AccessBlog.FileData(attachment.data.body, res.guid, res.mime)
-	                    fileUrl = metaweblog.new_media_object(fileData)
+                        #upload resource to blog
+                        fileData = AccessBlog.FileData(attachment.data.body, res.guid, res.mime)
+                        fileUrl = metaweblog.new_media_object(fileData)
 
-	                    #calculate hashcode for media
-	                    md5 = hashlib.md5()
-	                    md5.update(attachment.data.body)
-	                    hashcode = md5.hexdigest()
-	                    print("hast code: ", hashcode)
+                        #calculate hashcode for media
+                        md5 = hashlib.md5()
+                        md5.update(attachment.data.body)
+                        hashcode = md5.hexdigest()
+                        print("hast code: ", hashcode)
 
-	                    #replace en-media to img
-	                    content = evernote.replaceEnMediaWithImg(content, fileUrl.url, hashcode)
-	        except Exception as e:
-	            print(e)
-	        finally:
-	            pass
+                        #replace en-media to img
+                        content = evernote.replaceEnMediaWithImg(content, fileUrl.url, hashcode)
+            except Exception as e:
+                print(e)
+            finally:
+                pass
 
-	        #publish note to blog
-	        post = AccessBlog.Post(datetime.now(), content, n.title, tags)
-	        while True:
-		        try:
-		            metaweblog.new_post(post, True)
+            #publish note to blog
+            post = AccessBlog.Post(datetime.now(), content, n.title, tags)
+            while True:
+                try:
+                    metaweblog.new_post(post, True)
 
-		            print("publish note(%s) to blog successfully"%(n.title))
-		            break
-		        except Exception as e:
-		            print(e)
+                    print("publish note(%s) to blog successfully"%(n.title))
+                    break
+                except Exception as e:
+                    print(e)
 
-		            #reinit blog
-		            metaweblog = evernote.initBlog()
-		        finally:
-		            pass
+                    #reinit blog
+                    metaweblog = evernote.initBlog()
+                finally:
+                    pass
+                    
+            processedBlogCount = processedBlogCount + 1
+            print("processed %i blog"%(processedBlogCount))
 
-	        #write note to existed file, and do not sync it in next time
-	        existedBlog.write(n.title+'\n')
-	        existedBlog.flush()
+            #write note to existed file, and do not sync it in next time
+            existedBlog.write(n.title+'\n')
+            existedBlog.flush()
 
-	        time.sleep(30)
+            time.sleep(30)
 
-	        # #write note
-	        # try:
-	        #     f = open(n.guid+".html", "w+", encoding='utf-8-sig')
-	        #     f.write(content)
-	        #     f.close()
-	        # except Exception as err:
-	        #     print(err)
+            # #write note
+            # try:
+            #     f = open(n.guid+".html", "w+", encoding='utf-8-sig')
+            #     f.write(content)
+            #     f.close()
+            # except Exception as err:
+            #     print(err)
 
-	        print()
+            print()
 
-	    print()
+        print()
 
     else:
-    	print("get total notes of (%s) finished."%(notebook.name))
+        print("get total notes of (%s) finished."%(notebook.name))
 
-  	
+    
 existedBlog.close()
 
 print("publish notes to blog successfully")
